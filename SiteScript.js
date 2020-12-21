90941e517208ba1e080760281b71c206e1c3ca70
NoahDonohue <39565711+NoahDonohue@users.noreply.github.com>
2020-12-21 10:11:20 -0500

    autoCommit 2020 1Mo.101120

--- SiteScript.js
+++ SiteScript.js
@@ -1,9 +1,9 @@
-var blocks_high = ['0029400284', 'Shawn Bradley', 9, 1, 13, 0, 5, 'Pistons', 'H', '1994', 'https://www.basketball-reference.com/boxscores/1994012170PHI.html'];
-var steals_high = ['0028000378', 'Lionel Hollins', 14, 5, 0, 5, 0, 'Cavaliers', 'H', '1980', 'https://www.basketball-reference.com/boxscores/1980012170PHI.html'];
-var rebounds_high = ['0026600153', 'Wilt Chamberlain', 28, 12, 27, 0, 0, 'Pistons', 'H', '1966', 'https://www.basketball-reference.com/boxscores/1966012170PHI.html'];
-var assists_high = ['0025500078', 'George King', 20, 13, 8, 0, 0, 'Celtics', 'A', '1955', 'https://www.basketball-reference.com/boxscores/1955012170BOS.html'];
-var points_high = ['0026700189', 'Wilt Chamberlain', 47, 4, 26, 0, 0, 'Thunder', 'A', '1967', 'https://www.basketball-reference.com/boxscores/1967012170OKC.html', ['0027100268', 'Billy Cunningham', 47, 9, 13, 0, 0, 'Trail Blazers', 'A', '1971', 'https://www.basketball-reference.com/boxscores/1971012170POR.html']];
-var date = 'December 17th';
+var blocks_high = ['0029000320', 'Manute Bol', 6, 1, 8, 0, 5, 'Heat', 'A', '1990', 'https://www.basketball-reference.com/boxscores/1990012210MIA.html'];
+var steals_high = ['0028200296', 'Maurice Cheeks', 22, 9, 1, 6, 1, 'Celtics', 'H', '1982', 'https://www.basketball-reference.com/boxscores/1982012210PHI.html', ['0028300299', 'Maurice Cheeks', 16, 8, 1, 6, 0, 'Spurs', 'H', '1983', 'https://www.basketball-reference.com/boxscores/1983012210PHI.html']];
+var rebounds_high = ['0026600159', 'Wilt Chamberlain', 19, 11, 25, 0, 0, 'Lakers', 'A', '1966', 'https://www.basketball-reference.com/boxscores/1966012210LAL.html'];
+var assists_high = ['0028500320', 'Maurice Cheeks', 26, 14, 6, 4, 0, 'Celtics', 'H', '1985', 'https://www.basketball-reference.com/boxscores/1985012210PHI.html'];
+var points_high = ['0027900372', 'Julius Erving', 39, 5, 5, 2, 0, 'Rockets', 'H', '1979', 'https://www.basketball-reference.com/boxscores/1979012210PHI.html'];
+var date = 'December 21st';
 document.getElementById("date").innerHTML =  date;
 document.getElementById("PointsLink").href = points_high[10];
 document.getElementById("PointsPlayer").innerHTML = points_high[1]+ ": ";
